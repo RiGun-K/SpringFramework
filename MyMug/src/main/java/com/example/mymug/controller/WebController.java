7d5d72a45224693217d72027e6ad03a63a7b88a6
RiGun-K <1234suho@naver.com>
2021-09-07 00:21:37 +0900

    2021/09/07

---
+++ MyMug/src/main/java/com/example/mymug/controller/WebController.java
@@ -0,0 +1,53 @@
+package com.example.mymug.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+	/* 클라이언트에서 요청이 들어오면 (주소로 접속, 이벤트 요청 등)
+	 * Controller에서 요청을 처리한다 
+	 */
+	
+	// /templates/index.html을 반환
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title", "안녕하살법");
+		model.addAttribute("img1", "images/dd.jpg");
+		return "home";
+	}
+	
+	// Get 과 Post 함수 추가
+	@GetMapping("/update")
+	public String updateView() {
+		return "updatePoint";
+	}
+	
+	// name 이 중요하다고 하셨음
+	@PostMapping("/update")
+	public String updateResult(
+			@RequestParam(name="userid") String userid,
+			@RequestParam(name="point") int point,
+			Model model) {
+		model.addAttribute("result", userid + " " + point);
+		return "updatePoint";
+	}
+	
+	@GetMapping("/mypoint/{userid}")
+	public String myPoint(@PathVariable("userid") String userid, Model model) {
+		model.addAttribute("userid", userid);
+		model.addAttribute("point", 2000);
+		return "mypoint";
+	}
+
+}
