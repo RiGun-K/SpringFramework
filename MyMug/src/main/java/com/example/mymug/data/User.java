796a7dfd7c262cf8325a236381b8ff0d569c639a
RiGun-K <1234suho@naver.com>
2021-09-23 10:20:25 +0900

    2021/09/23

--- MyMug/src/main/java/com/example/mymug/data/User.java
+++ MyMug/src/main/java/com/example/mymug/data/User.java
@@ -1,16 +1,31 @@
 package com.example.mymug.data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	
+	@Id
 	private String userid;
+	@Column(nullable=false, length=100)
 	private String name;
+	private int point;
+	
 	public User(){}
 	public User(String userid, String name) {
 		this.userid = userid;
 		this.name = name;
 	}
+	
 	public String getUserid() { return userid; }
 	public void setUserid(String userid) { this.userid = userid; }
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; } 
+	public int getPoint() { return point; }
+	public void setPoint(int point) { this.point = point; }
 	
 }
