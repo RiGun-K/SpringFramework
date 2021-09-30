4e949b79dbc984b34366f8b87f4e83f32a445f47
RiGun-K <1234suho@naver.com>
2021-09-30 11:06:42 +0900

    2021/09/30

---
+++ AnyBoard/src/main/java/com/example/anyboard/AnyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.anyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyBoardApplication.class, args);
+	}
+
+}
