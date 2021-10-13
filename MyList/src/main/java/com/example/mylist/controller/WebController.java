ed252954050837d4183cb0f22914771319680892
RiGun-K <1234suho@naver.com>
2021-10-13 21:34:01 +0900

    2021/10/13

--- MyList/src/main/java/com/example/mylist/controller/WebController.java
+++ MyList/src/main/java/com/example/mylist/controller/WebController.java
@@ -3,6 +3,9 @@ package com.example.mylist.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
@@ -12,5 +15,26 @@ public class WebController {
 		model.addAttribute("title", "깃허브 연동 완료 ");
 		model.addAttribute("img1", "images/dd.jpg");
 		return "index";
+	}
+	
+	@GetMapping("/update")
+	public String updateView() {
+		return "updatePoint";
+	}
+	@PostMapping("/update")
+	public String updateResult(
+			@RequestParam(name="userid") String userid,
+			@RequestParam(name="point") int point,
+			Model model) {
+		model.addAttribute("result", userid + " " + point);
+		return "updatePoint";	
+	}
+	
+	
+	@GetMapping("/mypoint/{userid}")
+	public String myPoint(@PathVariable("userid") String userid, Model model) {
+			model.addAttribute("userid", userid);
+			model.addAttribute("point", 2000);
+			return "mypoint";
 	}
 }
---
+++ MyList/src/main/java/com/example/mylist/data/Point.java
@@ -0,0 +1,7 @@
+package com.example.mylist.data;
+
+// RESTful API 는 클래스 단위로 전달됨 ( 데이터용 클래스 )
+
+public class Point {
+
+}
