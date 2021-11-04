112762f560f630d17f6057ef230b10b48f1b800e
RiGun-K <1234suho@naver.com>
2021-11-04 10:11:11 +0900

    2021/11/04

---
+++ MyList/src/main/java/com/example/mylist/service/UserDetailsSericeImpl.java
@@ -0,0 +1,18 @@
+package com.example.mylist.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.mylist.repository.UserRepository;
+
+public class UserDetailsSericeImpl {
+
+}
